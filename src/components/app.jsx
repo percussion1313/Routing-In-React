ff0184083f2962b0639d01fab67d8060dcdbdd11
percussion1313 <peterjacobgrant@gmail.com>
2018-06-21 10:06:53 -0500

    First commit

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import logo from '../assets/logo.png'
+
+
+class App extends Component {
+    super(props) {
+        constructor(props)
+    }
+
+
+render() {
+    return <img src = {logo} alt="huh"/>
+}
+}
+
+
+export default App;
